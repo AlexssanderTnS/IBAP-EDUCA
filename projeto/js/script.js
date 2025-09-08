982eab559f28bacc67ddf578a9475aa759302ec8
AlexssanderTnS <alexssander.amat@gmail.com>
2025-09-08 11:01:01 -0300

    versão mobile

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -6,25 +6,25 @@ let slideInterval;
 
 function showTestimonial(index) {
     testimonials.forEach((t, i) => {
-    t.classList.toggle('active', i === index);
-});
+        t.classList.toggle('active', i === index);
+    });
 }
 
 
 
 function startSlideShow() {
-  // Limpa intervalo existente antes de criar novo
+    // Limpa intervalo existente antes de criar novo
     stopSlideShow();
     slideInterval = setInterval(() => {
-    currentIndex = (currentIndex + 1) % testimonials.length;
-    showTestimonial(currentIndex);
+        currentIndex = (currentIndex + 1) % testimonials.length;
+        showTestimonial(currentIndex);
     }, 7000);
 }
 
 function stopSlideShow() {
     if (slideInterval) {
-    clearInterval(slideInterval);
-}
+        clearInterval(slideInterval);
+    }
 }
 
 // Inicializa mostrando o primeiro testimonial
@@ -39,7 +39,7 @@ if (nextButton) {
         currentIndex = (currentIndex + 1) % testimonials.length;
         showTestimonial(currentIndex);
         startSlideShow(); // Reinicia o slideshow após interação
-});
+    });
 }
 
 if (prevButton) {
@@ -47,7 +47,7 @@ if (prevButton) {
         currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
         showTestimonial(currentIndex);
         startSlideShow(); // Reinicia o slideshow após interação
-});
+    });
 }
 
 // Inicia o slideshow automático
@@ -57,10 +57,46 @@ startSlideShow();
 
 //------------------------------------------------
 
-function clickMenu(){
-    if(itens.style.display == 'block'){
-        itens.style.display = 'none'
-    } else{
-        itens.style.display = 'block'
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+                    }s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
     }
 }
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".link",
+    ".link a"
+);
+mobileNavbar.init();
